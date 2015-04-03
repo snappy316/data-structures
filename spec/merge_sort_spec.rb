fe31a9fd59f0c62ef161fcfbf7cdb74c2562bdfa
David Ker <snappy316@gmail.com>
2015-04-03 12:31:05 -0700

    Rename pre-sorted array variable

--- spec/merge_sort_spec.rb
+++ spec/merge_sort_spec.rb
@@ -3,8 +3,8 @@ require 'merge_sort'
 
 describe 'merge Sort' do
   it 'should correctly sort numbers' do
-    insertion = [22, 3, 42, 1, 54, 56, 35]
-    sorted = insertion.merge_sort
+    to_sort = [22, 3, 42, 1, 54, 56, 35]
+    sorted = to_sort.merge_sort
     sorted.must_equal [1, 3, 22, 35, 42, 54, 56]
   end
 end
