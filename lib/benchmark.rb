0ad03fb9a5b7d4b2ed0c3612d1456168be7f589e
David Ker <snappy316@gmail.com>
2015-04-03 12:18:56 -0700

    Benchmark merge sort, and edit benchmark arrays

--- lib/benchmark.rb
+++ lib/benchmark.rb
@@ -1,12 +1,17 @@
 require 'benchmark'
 require_relative 'insertion_sort'
+require_relative 'merge_sort'
 
-random_arr = [2,9,28,15,43,44,25,35,33,9,6,26,15]
-nearly_sorted_arr = [3,3,5,11,17,11,12,11,25,25,28,32,33,39,41,42,44,46,50]
-reversed_arr = [25,20,15,10,5,3,2,1]
+random_arr = (1..10_000).map { rand(100) }
+sorted_arr = (1..10_000).to_a
+reversed_arr = (1..10_000).to_a.reverse
 
 Benchmark.bmbm do |x|
   x.report("Insertion sort - random") { random_arr.dup.insertion_sort }
-  x.report("Insertion sort - nearly sorted") { nearly_sorted_arr.dup.insertion_sort }
+  x.report("Insertion sort - sorted") { sorted_arr.dup.insertion_sort }
   x.report("Insertion sort - reversed") { reversed_arr.dup.insertion_sort }
+  x.report("Merge sort - random") { random_arr.dup.merge_sort }
+  x.report("Merge sort - sorted") { sorted_arr.dup.merge_sort }
+  x.report("Merge sort - reversed") { reversed_arr.dup.merge_sort }
+
 end
