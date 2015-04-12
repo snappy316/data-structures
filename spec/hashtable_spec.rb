0da0293f52edfa068897db45b3f92be80667e073
David Ker <snappy316@gmail.com>
2015-04-11 22:44:33 -0700

    Uncomment specs

--- spec/hashtable_spec.rb
+++ spec/hashtable_spec.rb
@@ -2,9 +2,9 @@ require_relative './spec_helper'
 require_relative '../lib/hashtable'
 
 describe 'Hashtable' do
-  # it 'should initialize as a fixed size' do
-  #   ht = Hashtable.new(1024)
-  # end
+  it 'should initialize as a fixed size' do
+    ht = Hashtable.new(1024)
+  end
 
   it 'should add new key/value pairs to the table' do
     ht = Hashtable.new(1024)
@@ -13,10 +13,10 @@ describe 'Hashtable' do
     val.must_equal('value')
   end
 
-  # it 'should tell me the size of the table' do
-  #   ht = Hashtable.new(1024)
-  #   ht.size.must_equal(0)
-  #   ht.set('key', 'value')
-  #   ht.size.must_equal(1)
-  # end
+  it 'should tell me the size of the table' do
+    ht = Hashtable.new(1024)
+    ht.size.must_equal(0)
+    ht.set('key', 'value')
+    ht.size.must_equal(1)
+  end
 end
